4198c988473ad13aceffa0918d7bace4b74d4ab3
Scott Luxton <78744890+ScottLuxton@users.noreply.github.com>
2021-03-30 15:08:49 -0600

    Add files via upload

--- skiPanorama.js
+++ skiPanorama.js
@@ -1,2 +1,37 @@
-console.log('Hello world')
+/*
+To do:
+Create Git repo - done, but is it right?
+Create local file and access in terminal - Done
+Create flow chart - Ongoing - How far to go? (start small)
+Build array of objects; ['ski pole', 'energy bar', 'etc..',]
+Build functions for all decisions
+Build functions for all actions
+Build functions to pick up / drop / use items
+
+Not sure about the expressJS, API, ReadMe and Node requirements yet
+*/
+
+let skiSunbird = true
+let pickUpPole = true
+let pickUpEnergyBar = true
+
+
+console.log("Decision - Begin on the Sunbird or Mile 1 chairlift?", skiSunbird) 
+if (skiSunbird) {
+    console.log("traverse to Sunbird chair")
+} else {
+    console.log("traverse to Mile 1 chair")  // return ends the program from running
+}
+
+console.log("HELLO WORLD")
+
+
+
+
+// console.log("Action - traverse to Sunbird, come across a lost ski pole on the trail, pick up?")
+// console.log("Decision - yes - pickup ski pole") //create an array for found objects
+// console.log("Action - continue to Sunbird chair")
+// console.log("Decision - Which trail, Fritz or Heaven can Wait") //answer Fritz
+// console.log("Action - You chose Fritz and came across a bear, get scared go home") //if you have pole defend yourself
+
 
