2e276879a1861eb02dd7b1ecad68044ed28c0baa
Scott Luxton <78744890+ScottLuxton@users.noreply.github.com>
2021-04-12 06:09:34 -0600

    Add files via upload

--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 
 This is my first evolveU project, build a choose your own adventrue game.
 
+//This page will need to be the instructions for others to be able to run the program
+
+
+
 
--- skiPanorama.js
+++ skiPanorama.js
@@ -4,34 +4,32 @@ Create Git repo - done, but is it right?
 Create local file and access in terminal - Done
 Create flow chart - Ongoing - How far to go? (start small)
 Build array of objects; ['ski pole', 'energy bar', 'etc..',]
-Build functions for all decisions
-Build functions for all actions
-Build functions to pick up / drop / use items
-
+Build functions for all decisions? - Turn decisions into functions
+Build functions for all actions?
+Build functions to pick up / drop / use items?
+Make sure functions are running not just showing up ie "undefined" "true" "false"
 Not sure about the expressJS, API, ReadMe and Node requirements yet
+
+module.export = {
+    functionName
+} 
+
+let {fileName} = require('./functionName)
+
 */
 
-let skiSunbird = true
-let pickUpPole = true
-let pickUpEnergyBar = true
+welcomeToPanorama ()
 
+const {rideSunbird, skiFritz} = require(`./skiPanoramaSunbird`)
+const {skiShowoff, rideMile1} = require(`./skiPanoramaMile1`)
 
-console.log("Decision - Begin on the Sunbird or Mile 1 chairlift?", skiSunbird) 
-if (skiSunbird) {
-    console.log("traverse to Sunbird chair")
-} else {
-    console.log("traverse to Mile 1 chair")  // return ends the program from running
+function welcomeToPanorama (name){
+    console.log("Welcome to Panorama")
 }
 
-console.log("HELLO WORLD")
 
 
 
 
-// console.log("Action - traverse to Sunbird, come across a lost ski pole on the trail, pick up?")
-// console.log("Decision - yes - pickup ski pole") //create an array for found objects
-// console.log("Action - continue to Sunbird chair")
-// console.log("Decision - Which trail, Fritz or Heaven can Wait") //answer Fritz
-// console.log("Action - You chose Fritz and came across a bear, get scared go home") //if you have pole defend yourself
 
 
